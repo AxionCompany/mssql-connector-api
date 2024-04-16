2b3a3389be91926311338197d57901b47d17e766
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2024-04-16 14:21:51 -0300

    first commit

---
+++ dockerfile
@@ -0,0 +1,25 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# # Install FreeTDS and other dependencies you might need
+# RUN apt-get update && apt-get install -y \
+#     freetds-dev \
+#     freetds-bin \
+#     && rm -rf /var/lib/apt/lists/*
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 9000 available to the world outside this container
+EXPOSE 9000
+
+# Define environment variable
+
+# Run app.py when the container launches
+CMD ["python", "./main.py"]
---
+++ main.py
@@ -0,0 +1,60 @@
+import pymssql
+import json
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/teste', methods=['POST'])
+def teste():
+    user = request.headers.get('user')
+    password = request.headers.get('password')
+    port = request.headers.get('port', '1433')  # Default SQL Server port is 1433
+    server = request.headers.get('server')
+    database = request.headers.get('database')
+
+    if not all([user, password, server]):
+        return jsonify({'status': 'error', 'message': 'Missing Headers. Make sure you\'ve set `user`, `password`, `server` headers'}), 400
+
+    name = request.json.get('name')
+    params = request.json.get('params')
+    __filterTable__ = request.json.get('__filterTable__')
+
+    if __filterTable__ is None:
+        __filterTable__ = 0
+
+    if not name:
+        return jsonify({'status': 'error', 'message': 'Missing Parameters. Make sure you\'ve set `name` parameter'}), 400
+
+    conn = None  # Initialize conn before the try block
+    try:
+        conn = pymssql.connect(server=server, user=user, password=password, database=database, port=port, as_dict=True)
+        cursor = conn.cursor()
+        
+        exec_statement = f"EXEC {name} " + ", ".join([f"@{k}=%s" for k in params.keys()])
+        cursor.execute(exec_statement, tuple(params.values()))
+
+        current_set_index = 0
+        data = [] # Initialize data with an empty array
+
+        # Iterate through result sets until the desired one is found or until there are no more
+        while True:
+            if current_set_index == __filterTable__:
+                data = [row for row in cursor.fetchall()]
+                break
+            if not cursor.nextset():
+                break  # No more result sets available
+            current_set_index += 1
+        df = pd.DataFrame(data)
+        # Convert the DataFrame to JSON and wrap it in a 'data' object
+        response_data = {'data': json.loads(df.to_json(orient='records', date_format='iso'))}
+        return jsonify(response_data)
+    except Exception as e:
+        print(e)
+        return jsonify({'status': 'error', 'message': str(e)}), 400
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)
