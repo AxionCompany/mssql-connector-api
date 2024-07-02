c64b5696cca5ddd7ccbf6c0c1d432adcbc27d9d5
Giovanni Romano Neto <giovanni.romano@dex.company>
2024-07-02 10:15:24 -0300

    fix: Remove callback feature

--- main.py
+++ main.py
@@ -3,45 +3,8 @@ import json
 from flask import Flask, request, jsonify
 import pandas as pd
 import os
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
-
-executor = ThreadPoolExecutor(max_workers=5)
-
-async def execute_code_and_return_response(name, params, __filterTable__, server, user, password, database, port):
-    with app.app_context():  # Push application context manually
-        conn = None  # Initialize conn before the try block
-        try:
-            conn = pymssql.connect(server=server, user=user, password=password, database=database, port=port, as_dict=True)
-            cursor = conn.cursor()
-
-            exec_statement = f"EXEC {name} " + ", ".join([f"@{k}=%s" for k in params.keys()])
-            cursor.execute(exec_statement, tuple(params.values()))
-
-            current_set_index = 0
-            data = [] # Initialize data with an empty array
-
-            # Iterate through result sets until the desired one is found or until there are no more
-            while True:
-                if current_set_index == __filterTable__:
-                    data = [row for row in cursor.fetchall()]
-                    break
-                if not cursor.nextset():
-                    break  # No more result sets available
-                current_set_index += 1
-            df = pd.DataFrame(data)
-            # Convert the DataFrame to JSON and wrap it in a 'data' object
-            response_data = {'data': json.loads(df.to_json(orient='records', date_format='iso'))}
-            return jsonify(response_data)
-        except Exception as e:
-            print(e)
-            return jsonify({'status': 'error', 'message': str(e)}), 400
-        finally:
-            if conn:
-                conn.close()
-
 # create a health check route
 @app.route('/', methods=['GET'])
 def health():
@@ -54,13 +17,12 @@ def teste():
     port = request.headers.get('port', '1433')  # Default SQL Server port is 1433
     server = request.headers.get('server')
     database = request.headers.get('database')
-    
+
     if not all([user, password, server]):
         return jsonify({'status': 'error', 'message': 'Missing Headers. Make sure you\'ve set `user`, `password`, `server` headers'}), 400
 
     name = request.json.get('name')
     params = request.json.get('params')
-    wait = request.json.get('wait', True)
     __filterTable__ = request.json.get('__filterTable__')
 
     if __filterTable__ is None:
@@ -69,12 +31,35 @@ def teste():
     if not name:
         return jsonify({'status': 'error', 'message': 'Missing Parameters. Make sure you\'ve set `name` parameter'}), 400
 
-    if wait:
-        return asyncio.run(execute_code_and_return_response(name, params, __filterTable__, server, user, password, database, port))
-    else:
-        loop = asyncio.get_event_loop()
-        loop.run_in_executor(executor, execute_code_and_return_response, name, params, __filterTable__, server, user, password, database, port)
-        return jsonify({'status': 'ok', 'message': 'Procedure started ' + name +' successfly'})
+    conn = None  # Initialize conn before the try block
+    try:
+        conn = pymssql.connect(server=server, user=user, password=password, database=database, port=port, as_dict=True)
+        cursor = conn.cursor()
+        
+        exec_statement = f"EXEC {name} " + ", ".join([f"@{k}=%s" for k in params.keys()])
+        cursor.execute(exec_statement, tuple(params.values()))
+
+        current_set_index = 0
+        data = [] # Initialize data with an empty array
+
+        # Iterate through result sets until the desired one is found or until there are no more
+        while True:
+            if current_set_index == __filterTable__:
+                data = [row for row in cursor.fetchall()]
+                break
+            if not cursor.nextset():
+                break  # No more result sets available
+            current_set_index += 1
+        df = pd.DataFrame(data)
+        # Convert the DataFrame to JSON and wrap it in a 'data' object
+        response_data = {'data': json.loads(df.to_json(orient='records', date_format='iso'))}
+        return jsonify(response_data)
+    except Exception as e:
+        print(e)
+        return jsonify({'status': 'error', 'message': str(e)}), 400
+    finally:
+        if conn:
+            conn.close()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 9000)))
